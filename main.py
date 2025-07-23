2d1b870a9144b28253c3a29b7377ddfd062a860a
ImaStudy <kubatchuitenov254@gmail.com>
2025-07-23 11:58:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,5 @@ def handle_chat(message):
 
 # === Запуск ===
 if __name__ == '__main__':
-    Thread(target=run_flask).start()
-    bot.remove_webhook()
-    time.sleep(10)
-    bot.polling(none_stop=True)
+   Thread(target=bot.infinity_polling).start()
+run_flask()
